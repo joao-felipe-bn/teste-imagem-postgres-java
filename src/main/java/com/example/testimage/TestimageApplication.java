460d90aac3a0a02128c66b6eba20cc937d16f562
joao-felipe-bn <jfbnfelipe.jfbn@gmail.com>
2023-06-25 11:20:59 -0300

    Revert "study about streams java"
    This reverts commit 2bd9eaf8da1028bbdab51f3892f9fc83204d3c33.

---
+++ src/main/java/com/example/testimage/TestimageApplication.java
@@ -0,0 +1,13 @@
+package com.example.testimage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestimageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestimageApplication.class, args);
+	}
+
+}
